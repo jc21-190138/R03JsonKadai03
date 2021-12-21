d62e19a19c31749ca9a30d91ed3ca50e12a92f5a
成喜 外塚 <190089@jc-21.jp>
2021-12-21 10:03:40 +0900

    getpont　更新　DBからデータの取り出し（失敗)12/21

--- WebContent/WEB-INF/jsp/getPoint.jsp
+++ WebContent/WEB-INF/jsp/getPoint.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="application/json; charset=UTF-8"
     pageEncoding="UTF-8"%>
- <%
+<%
     int POINT = (int)request.getAttribute("POINT");
 %>
 {"POINT":3580}
--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -1,13 +1,18 @@
 
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-import javax.servlet.RequestDispatcher;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
 
 /**
  * Servlet implementation class GetPointServlet
@@ -15,31 +20,43 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/getPoint")
 public class GetPointServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetPointServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @ see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-   
-    
-    
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		int point=123;
-		
-		request.setAttribute("POINT",point);
-		
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
-		rd.forward(request, response);
+	public GetPointServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @ see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 * response)
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource) ic.lookup("java:/comp/env/jdbc/jsonkadai03");
+			Connection con = ds.getConnection();
+			String keyword = request.getParameter("keyword");
+			PreparedStatement st = con.prepareStatement("select * from point_table");
+			st.setString(1, "%" + keyword + "%");
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				System.out.println(rs.getString("shop_id"));
+				System.out.println(":");
+				System.out.println(rs.getString("user_id"));
+				System.out.println(":");
+				System.out.println(rs.getInt("point"));
+			}
+			st.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
