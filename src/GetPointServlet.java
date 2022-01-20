5d8d3e4678bf64c82fc520ac82aed02d7b613465
成喜 外塚 <190089@jc-21.jp>
2022-01-20 15:06:15 +0900

    指定したpointを入手可能

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -1,10 +1,14 @@
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.InitialContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +30,7 @@ public class GetPointServlet extends HttpServlet {
 	 */
 	public GetPointServlet() {
 		super();
-		// TODO Auto-generated constructor stub
+// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -36,27 +40,75 @@ public class GetPointServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		response.setContentType("text/html; charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
 		try {
 			InitialContext ic = new InitialContext();
-			DataSource ds = (DataSource) ic.lookup("java:/comp/env/jdbc/jsonkadai03");
+			DataSource ds = (DataSource)ic.lookup(
+					"java:/comp/env/jdbc/jsonkadai03");
 			Connection con = ds.getConnection();
-			String keyword = request.getParameter("keyword");
-			PreparedStatement st = con.prepareStatement("select * from point_table");
-			st.setString(1, "%" + keyword + "%");
-			ResultSet rs = st.executeQuery();
-			while (rs.next()) {
-				System.out.println(rs.getString("shop_id"));
-				System.out.println(":");
-				System.out.println(rs.getString("user_id"));
-				System.out.println(":");
-				System.out.println(rs.getInt("point"));
+			String point=null;
+		//String userid = "ABCDEFGHIJK1234";
+		//String tenpoid = "1234567890";
+		
+		/*final String driverName = "com.mysql.jdbc.Driver";
+		final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai03";
+		final String id = "jsonkadai03";
+		final String pass = "JsonKadai03";*/
+
+		//try {
+			/*Class.forName(driverName);
+			Connection connection=DriverManager.getConnection(url,id,pass);
+			PreparedStatement st1 =
+					con.prepareStatement(
+							"SELECT * FROM point_table WHERE tenpo_id = 1 "
+						);
+			ResultSet result1 = st1.executeQuery();
+			List<String[]> list1 = new ArrayList<>();
+			while( result1.next() == true) {
+				String[] s = new String[1];
+				s[0]=result1.getString("point");
+				list1.add(s);
+				
+			}*/
+			
+			
+		PreparedStatement st = con.prepareStatement("SELECT * FROM point_table WHERE tenpo_id = ? and user_id=?");
+		
+		st.setString(1,"2");//ここにqrコードのデータを入れる
+		st.setString(2,"3");
+		ResultSet rs=st.executeQuery();
+		
+		/*List<String[]> list = new ArrayList<>();
+		while( rs.next() == true) {
+			String[] s = new String[1];
+			s[0]=rs.getString("point");
+			list.add(s);
+		}*/
+		
+		if(rs.next()){
+			point= rs.getString("point");
 			}
-			st.close();
-			con.close();
+		//String point= rs.getString("point");
+		
+		
+		//st.close();
+		//con.close();
+		
+		request.setAttribute("POINT", point);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+		rd.forward(request, response);
 		} catch (Exception e) {
-			e.printStackTrace();
+			out.println("<pre>");
+			e.printStackTrace(out);
 		}
 	}
-//aa
 }
+	
+		
+		
+
+		
+
